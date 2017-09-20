fe899689b74be66e2f26c9e001b1f1c1db463ff8
Anna <espro@mail.ru>
2017-09-20 22:33:27 +0300

    Multiply function implemented without 3d party library

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-var bigInt = require("big-integer");
-
 module.exports = function multiply(first, second) {
-var firstNum = bigInt(first);
-var secondNum = bigInt(second);
- var answer = firstNum.multiply(secondNum);
- return answer.toString();
+    var reversedArray1 = first.split("").reverse();
+    var reversedArray2 = second.split("").reverse();
+    var resultArray = new Array;
+ 
+    for ( var i = 0; i < reversedArray1.length; i++ ) {
+        for ( var k = 0; k < reversedArray2.length; k++ ) {
+            var resultIndex = i + k;
+            resultArray[resultIndex] = reversedArray1[i] * reversedArray2[k] + ( resultIndex >= resultArray.length ? 0 : resultArray[resultIndex] );
+ 
+            if ( resultArray[resultIndex] > 9 ) {
+                resultArray[resultIndex + 1] = Math.floor( resultArray[resultIndex] / 10 ) + ( resultIndex + 1 >= resultArray.length ? 0 : resultArray[resultIndex + 1] );
+                resultArray[resultIndex] -= Math.floor( resultArray[resultIndex] / 10 ) * 10;
+            }
+        }
+    }
+    return resultArray.reverse().join("");
 }
