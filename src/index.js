21270062939dac0c0e36a4efc0f892b9bde74526
Anna Bogdanova <espro@mail.ru>
2017-10-06 22:36:03 +0300

    Optimizing the code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 module.exports = function multiply(first, second) {
     var reversedArray1 = first.split("").reverse();
     var reversedArray2 = second.split("").reverse();
-    var resultArray = new Array;
+    var resultArray = [];
+    var length1 = reversedArray1.length;
+    var length2 = reversedArray2.length;
  
-    for ( var i = 0; i < reversedArray1.length; i++ ) {
-        for ( var k = 0; k < reversedArray2.length; k++ ) {
+    for ( var i = 0; i < length1; i++ ) {
+        for ( var k = 0; k < length2; k++ ) {
             var resultIndex = i + k;
             resultArray[resultIndex] = reversedArray1[i] * reversedArray2[k] + ( resultIndex >= resultArray.length ? 0 : resultArray[resultIndex] );
  
             if ( resultArray[resultIndex] > 9 ) {
                 resultArray[resultIndex + 1] = Math.floor( resultArray[resultIndex] / 10 ) + ( resultIndex + 1 >= resultArray.length ? 0 : resultArray[resultIndex + 1] );
-                resultArray[resultIndex] -= Math.floor( resultArray[resultIndex] / 10 ) * 10;
+                resultArray[resultIndex] = resultArray[resultIndex] % 10;
             }
         }
     }
     return resultArray.reverse().join("");
-}
